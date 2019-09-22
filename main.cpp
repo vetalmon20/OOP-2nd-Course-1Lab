70aa65724454cd86af40b0e9a9598fa9cd789488
Vitaliy Datsiuk <vetalvetal222@gmail.com>
2019-09-22 16:02:29 +0300

    Added 2 sorts
    Added bucket and counting sort.
    Also added the function of concatenating, but it should be fixed.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
  * Task: make a structure of linked list and realise different sort algorithms
  *
  *@author Vitaliy Datsiuk
- *@version 1.1 16/09/19
+ *@version 1.3 22/09/19
  */
 
 #include <iostream>
@@ -27,6 +27,7 @@ private:
     Node <T> *head, *tail;                     //two pointers on the first and the last node
     int node_sum;                              //number of the nodes is list
 
+    //swaps the data between nodes
     void swap(Node <T> *a, Node <T> *b) {
 
         //cathing the possible error
@@ -39,8 +40,17 @@ private:
         temp = a->data;
         a->data = b->data;
         b->data = temp;
-    }   //swaps the data between nodes
+    }
 
+    Node<T>* concantenate(List<T> first, List<T> second){
+        Node<T>* tail = first.get_tail();
+        Node<T>* head = second.get_head();
+        tail->next = head;
+
+        return first.get_head();
+    }
+
+    //utility function for quicksort
     Node <T>* partition_quicksort(Node <T>* start, Node <T>* finish){
 
         T piv_data = finish->data;      //data that pivot contain
@@ -66,8 +76,9 @@ private:
         swap (temp, j);
 
         return temp;
-    } //util.funct.for quicksort
+    }
 
+    //actually quicksort
     void _quicksort(Node <T>* start, Node <T>* finish){
 
         if (finish != NULL && start != finish && start != finish->next){
@@ -75,7 +86,7 @@ private:
             _quicksort(start, pivot->prev);
             _quicksort(pivot->next, finish);
         }
-    }    //actually quicksort
+    }
 
     //splits the list on two halves. Used in mergesort
     void listsplit_mergesort(Node <T> *start, Node <T> **first, Node <T> **second){
@@ -126,6 +137,7 @@ private:
         return (result);
     }
 
+    //actually mergesort
     void _mergesort(Node <T> **source)
     {
         Node <T> *start = *source;
@@ -159,6 +171,13 @@ private:
         tail = temp;
     }
 
+    //utility function for countingsort that fill the input array with 0
+    void fill_array_0(int in[], int length){
+        for (int i = 0; i < length; i++){
+            in[i] = 0;
+        }
+    }
+
 public:
     List(){
 
@@ -174,6 +193,7 @@ public:
         return head;
     }
 
+    //returns number of nodes
     int get_node_sum(){
         return node_sum;
     }
@@ -198,6 +218,7 @@ public:
         node_sum ++;                    //updated the number of the nodes
     }
 
+    //printing all the data from the list on the screen
     virtual void display(Node <T> *start){
 
         if(start != NULL)
@@ -205,7 +226,7 @@ public:
             cout << start->data << " ";
             display(start->next);
         }
-    }  //printing all the data from the list on the screen
+    }
 
     void insertion_sort(){
 
@@ -223,13 +244,109 @@ public:
         }
     }
 
+    //just calls the recursive _quicksort function in private
     void quicksort(){
         _quicksort(head,tail);  //calls the function
-    }           //just calls the recursive _quicksort function in private
+    }
 
+    //just calls the recursive _quicksort function in private
     void mergesort(){
         _mergesort(&head);
-    }           //just calls the recursive _quicksort function in private
+    }
+
+    //this sorting algorithm is valid only for the integers in [0,50)
+    void countingsort(){
+
+        int max_int = 50;     //I use "49" as maximum possible value to the given integer
+
+        if (typeid(T).name() != typeid(int).name()){
+            cout << "Sorry, your values are not integers"<<endl;    //if elements are not int
+            return;
+        }
+
+        Node <T>* start = head;
+        int num = get_node_sum();
+        if (num < 2){
+            cout<<"nothing to sort"<<endl;                  //if number of elements are 0 or 1
+            return;
+        }
+
+        int index[max_int];
+        fill_array_0(index,max_int);                    //creating index array
+
+        int out[num];                                   //output array
+        int temp;
+
+        //creating the index array
+        while (start != NULL){
+            temp = start->data;
+            index[temp]++;                      //incrementing the index val of the current value
+            start = start->next;
+        }
+
+        //modifying the index array
+        for (int i = 1; i<max_int; i++){
+            index[i] = index[i] + index[i - 1];
+        }
+
+        //creating the output array using index array
+        start = head;
+        while (start != NULL){
+            temp = start->data;
+            out[index[temp] - 1] = temp;
+            index[temp]--;
+            start = start->next;
+        }
+
+        //rewriting the values in the linked list from the output array
+        start = head;
+        for(int k=0;k<num;k++) {
+            start->data = out[k];
+            start = start->next;
+        }
+    }
+
+    //this sorting algorithm is valid only for the integers in [0,100)
+    void bucketsort(){
+
+        if (typeid(T).name() != typeid(int).name()){
+            cout << "Sorry, your values are not integers"<<endl;    //if elements are not int
+            return;
+        }
+        if (get_node_sum() < 2){
+            cout<<"nothing to sort"<<endl;                  //if number of elements are 0 or 1
+            return;
+        }
+
+        List<int> arr[10];
+        Node <T>* start = head;
+        int base;                            //the basis dividing
+
+        //determine the index of the integer to put in
+        while (start != NULL){
+            base = start->data / 10;
+            arr[base].add_node(start->data);
+            start = start->next;
+        }
+
+        //sorting the every list in array
+        for (int i=0; i<10; i++) {
+            arr[i].insertion_sort();
+        }
+
+        start = head;
+        Node<T> * sec_start;                //used for parsing the array
+
+        //rewriting the information in the main list
+        for (int k=0; k<10; k++) {
+            sec_start = arr[k].get_head();
+            while (sec_start != NULL){
+                start->data = sec_start->data;
+                sec_start = sec_start->next;
+                start = start->next;
+            }
+        }
+    }
 
 };
 
@@ -253,19 +370,22 @@ int main() {
 
 
     List<int> i;
-    i.add_node(4);
-    i.add_node(5);
-    i.add_node(3);
-    i.add_node(2);
-    i.add_node(10);
-    i.add_node(7);
+    i.add_node(42);
+    i.add_node(53);
+    i.add_node(31);
+    i.add_node(22);
+    i.add_node(20);
+    i.add_node(33);
     i.display(i.get_head());
     cout<<endl;
     //i.quicksort();
-    //i.insertion_sort();
-    Node <int> *SA=i.get_head();
-    i.mergesort();
+    //i.insertion_sort();;
+    //i.mergesort();
+    //i.countingsort();
+    i.bucketsort();
     i.display(i.get_head());
+
+
 
     // cout<<"number of the nodes is:"<<i.get_node_sum()<<endl;
 
