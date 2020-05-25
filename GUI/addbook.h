2932b1153bf16300ee3bf77b3e381f895cdf4da8
Vitaliy Datsiuk <vetalvetal222@gmail.com>
2020-05-25 22:27:14 +0300

    small fixes

--- GUI/addbook.h
+++ GUI/addbook.h
@@ -8,6 +8,9 @@ namespace Ui {
 class addbook;
 }
 
+/**
+ * @brief The addbook class is used to create a window with input data to create the book
+ */
 class addbook : public QDialog
 {
     Q_OBJECT
@@ -22,11 +25,21 @@ private slots:
     void on_ok_clicked();
 
 signals:
+    /**
+     * @brief entered_value send the signal with the input value and the boolean type that shows the correction of the input
+     * @param input input string
+     * @param checked if the data is correct
+     * @param date input date
+     */
     void entered_value(std::string input, bool checked, QDate date);
 
 private:
     Ui::addbook *ui;
 
+    /**
+     * @brief check_input checks the input
+     * @return 1 if the data is correct, vice versa returns 0
+     */
     bool check_input();
 };
 
--- GUI/addvalue.h
+++ GUI/addvalue.h
@@ -19,9 +19,18 @@ public:
     explicit addvalue(QWidget *parent = nullptr);
     ~addvalue();
 signals:
+    /**
+     * @brief entered_value send the signal with the input value and the boolean type that shows the correction of the input
+     * @param input input string
+     * @param checked if the data is correct
+     */
     void entered_value(string input, bool checked);
 
 public slots:
+    /**
+     * @brief set_type sets the type of the list to this class
+     * @param type the type of the array
+     */
     void set_type(int type);
 
 private slots:
@@ -32,9 +41,18 @@ private:
     Ui::addvalue *ui;
     int type;
 
+    /**
+     * @brief check_input checks the input
+     * @return 1 if the data is correct, vice versa returns 0
+     */
     bool check_input();
 };
 
+/**
+ * @brief isInteger isFloat isChar are func that checks the correction of the input for respective types of data
+ * @param in the input string
+ * @return 1 if the data is correct. vice versa returns 0
+ */
 bool isInteger(const std::string & in);
 bool isFloat(const std::string & in);
 bool isChar(const std::string & in);
